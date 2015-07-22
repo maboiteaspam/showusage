426812e61143156e5e801eaa0412cb29adf7e471
clement <maboiteaspam@wanadoo.fr>
2015-07-23 00:49:00 +0800

    Fix #1: It is now compatible with windows It is now using npm bin rather than it s node API Adds dependency to byline to split stdout

--- index.js
+++ index.js
@@ -1,29 +1,61 @@
 #!/usr/bin/env node
 
-var pkgToRead = process.argv[2] || 'showusage'
-var section = new RegExp(process.argv[3]) || 'usage'
-var npm = require('npm')
 var path = require('path')
+var fs = require('fs')
+var byline = require('byline')
 var mdUtils = require('md-stream-utils')
 var resumer = require('resumer')
+var spawn = require('child_process').spawn
 
-npm.load({global: true, depth: 1},function(err, npm) {
-  npm.commands.ls([pkgToRead], true, function(err, data) {
-    data = data.dependencies[pkgToRead];
-    if (!data){
-      console.error('There is no such package: '+pkgToRead)
-    }else if (data.readme) {
-      resumer()
-        .queue(data.readme)
-        .pipe(mdUtils.tokenizer())
-        .pipe(mdUtils.byParapgraph())
-        .pipe(mdUtils.filter({content: /\s+Usage/}))
-        .pipe(mdUtils.cliColorize())
-        .pipe(mdUtils.toString())
-        .pipe(process.stdout)
+var pkgToRead = process.argv[2] || 'showusage'
+var section = new RegExp(process.argv[3]) || 'usage'
 
-    } else {
-      console.error('There is no README in this package')
-    }
-  });
-});
+var npmHasFound = false
+var npmError = false
+var npmLineIndex = 0
+var npmPkgHome = false
+
+var npm = spawn('npm', ['ls', pkgToRead, '-g', '--depth=1'])
+
+npm.on('error', function(e){
+  npmError = e
+})
+
+byline(npm.stdout).on('data', function(d){
+  d = ''+d
+  if( npmLineIndex === 0 ){
+    npmPkgHome = d
+  } else if (d.match(/\(empty\)/)) {
+    npmHasFound = false
+  } else {
+    npmHasFound = true
+  }
+  npmLineIndex++;
+})
+
+npm.on('close', function(){
+  if (npmError) {
+    console.error('NPM got error')
+    console.error(npmError)
+  }
+  if (!npmHasFound) {
+    return console.error('There is no such package: '+pkgToRead)
+  }
+  if (fs.existsSync(npmPkgHome) === false) {
+    return console.error('There is no such directory: '+npmPkgHome)
+  }
+  var data = require( path.join(npmPkgHome, 'node_modules', pkgToRead, 'package.json'))
+  if (!data.readme) {
+    console.error('There is no README in this package')
+
+  } else {
+    resumer()
+      .queue(data.readme)
+      .pipe(mdUtils.tokenizer())
+      .pipe(mdUtils.byParapgraph())
+      .pipe(mdUtils.filter({content: /\s+Usage/}))
+      .pipe(mdUtils.cliColorize())
+      .pipe(mdUtils.toString())
+      .pipe(process.stdout)
+  }
+})
