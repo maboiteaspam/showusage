8f13b98a7330cd9ca46d8d6a9c0d373cb7889766
clement <maboiteaspam@wanadoo.fr>
2015-08-09 19:52:03 +0800

    Remove resumer dep, it was not managing flush sequence properly When the Usage section can not be found within the README the program will find al headings and invite the user to select the section to read The user can also select a special option to read the whole README

--- index.js
+++ index.js
@@ -6,70 +6,95 @@ var through2 = require('through2')
 var glob = require('glob')
 var byline = require('byline')
 var mdUtils = require('md-stream-utils')
-var resumer = require('resumer')
+var inquirer = require("inquirer");
 var exec = require('child_process').exec
 
 var pkgToRead = process.argv[2] || 'showusage'
-var section = process.argv[3] || '\\s+Usage'
+var sectionToFind = process.argv[3] || '\\s+Usage'
 var global = !fs.existsSync(path.join(pkgToRead, 'package.json'))
   && !fs.existsSync(path.join('.', 'node_modules', pkgToRead))
 
 if (global) {
   npmPkgDir(pkgToRead, function(err, npmPkgHome){
     if (err) return console.error(err)
-    showREADMESection (npmPkgHome, pkgToRead, section)
+    showREADMESection (npmPkgHome, pkgToRead, sectionToFind)
   })
 } else {
-  showREADMESection (process.cwd(), pkgToRead, section)
+  showREADMESection (process.cwd(), pkgToRead, sectionToFind)
 }
 
-function showREADMESection (npmPkgHome, pkgToRead, section) {
+function showREADMESection (npmPkgHome, pkgToRead, sectionToFind) {
 
-  var data = {readme: ''}
-  var basePath = ''
+  var pkgPath = findPackagePath(npmPkgHome, pkgToRead)
+  if (!pkgPath) {
+    return console.error('Can\'t find package \''+pkgToRead+'\'')
+  }
+  var READMEContent = getREADMEContent(pkgPath)
+  if (!pkgPath) {
+    return console.error('Can\'t find README file for \''+pkgToRead+'\'')
+  }
+
+  getOneParagrah(READMEContent, sectionToFind, function (err) {
+    if (err) {
+      return getAllHeadings(READMEContent, function (headings) {
+        var suggestions = ['All the content']
+        headings.forEach(function (head) {
+          suggestions.push(head.content)
+        })
+        suggestions.push(new inquirer.Separator())
+        var message = ''
+          + err + '\n'
+          + 'Would you like to read an alternative section ?' + ''
+        suggestAlternativeReadings(message, suggestions, function (answer) {
+          if (answer.match(/^All the content$/)) {
+            var stream = toCharacter();
+            stream.pipe(mdUtils.tokenizer())
+              .pipe(mdUtils.cliColorize())
+              .pipe(mdUtils.toString())
+              .pipe(process.stdout);
+            stream.write(READMEContent)
+          } else {
+            getOneParagrah(READMEContent, answer).pipe(process.stdout).resume()
+          }
+        })
+      }).resume()
+    }
+  }).pipe(process.stdout).resume()
+}
+
+function findPackagePath (npmPkgHome, pkgToRead) {
+
+  var basePath = false
   if (fs.existsSync(path.join(npmPkgHome, 'node_modules', pkgToRead, 'package.json'))){
-    data = require(path.join(npmPkgHome, 'node_modules', pkgToRead, 'package.json'))
     basePath = path.join(npmPkgHome, 'node_modules', pkgToRead)
 
   }else if (fs.existsSync(pkgToRead, 'package.json')){
-    data = require(path.join(pkgToRead, 'package.json'))
     basePath = path.join(pkgToRead)
 
   }else{
     throw 'package not found'
   }
 
-  var READMEContent
+  return basePath
+}
+
+function getREADMEContent (npmPkgPath) {
+
+  var READMEContent = false
+  var packagePath = path.join(npmPkgPath, 'package.json')
+  var data = require(packagePath) || {readme: ''}
 
   if (!data.readme || data.readme==='ERROR: No README data found!') {
-    var READMEfile = glob.sync('README**', {nodir: true, nocase: true, cwd: basePath})
-    if (!READMEfile.length || !fs.existsSync(path.join(basePath, READMEfile[0]))) {
-      return console.log('There is no README in this package')
+    var READMEfile = glob.sync('README**', {nodir: true, nocase: true, cwd: npmPkgPath})
+    var READMEPath = path.join(npmPkgPath, READMEfile[0])
+    if (READMEfile.length && fs.existsSync(READMEPath)) {
+      READMEContent = fs.readFileSync(READMEPath)
     }
-    READMEContent = fs.readFileSync(path.join(basePath, READMEfile[0]))
   } else {
     READMEContent = data.readme
   }
 
-  var found = false
-
-  resumer()
-    .queue(READMEContent)
-    .pipe(mdUtils.tokenizer())
-    .pipe(mdUtils.byParapgraph())
-    .pipe(mdUtils.filter({content: new RegExp(section)}))
-    .pipe(mdUtils.cliColorize())
-    .pipe(mdUtils.toString())
-    .pipe(through2(function (chunk, enc, callback) {
-      found = true
-      this.push(chunk)
-      callback()
-    }))
-    .pipe(process.stdout)
-
-  process.on('beforeExit', function() {
-    if (!found) console.error('This README does not have such section ' + section)
-  });
+  return READMEContent
 }
 
 function npmPkgDir (pkgToRead, then) {
@@ -85,4 +110,88 @@ function npmPkgDir (pkgToRead, then) {
 
     then(null, npmPkgHome)
   })
+}
+
+function toCharacter () {
+  return through2(function (chunk, enc, callback) {
+    chunk = '' + chunk
+    for(var i=0;i<chunk.length;i++) {
+      this.push(chunk.substr(i, 1))
+    }
+    this.push( null );
+    callback()
+  })
+}
+
+function getOneParagrah(READMEContent, section, then){
+  var paragraph = ''
+  var stream = toCharacter();
+  var tokenized = stream
+    .pipe(mdUtils.tokenizer())
+    .pipe(mdUtils.byParapgraph())
+    .pipe(mdUtils.filter({content: new RegExp(section)}))
+    .pipe(mdUtils.cliColorize())
+    .pipe(mdUtils.toString())
+    .pipe((function(){
+      var index = 0
+      return through2(function (chunk, enc, callback) {
+        index++;
+        if (then) paragraph+='' + chunk
+        this.push(chunk)
+        callback()
+      }, function(callback){
+        var err = !index
+          ? 'This README does not have such section ' + section
+          : null;
+        if (then) then(err, paragraph)
+        callback()
+      })
+    })())
+  tokenized.pause()
+  stream.write(READMEContent)
+  return tokenized
+}
+
+function getAllHeadings(READMEContent, then){
+  var headings = []
+  var stream = toCharacter();
+  var tokenized =
+    stream.pipe(mdUtils.tokenizer())
+    .pipe(mdUtils.byLine())
+    .pipe(through2.obj(function (chunk, enc, callback) {
+      if (chunk[0].type==='heading') {
+        var str = {
+          power: chunk[0].content.length,
+          content: ''
+        }
+        chunk.forEach(function (c) {
+          if (['text','whitespace'].indexOf(c.type)>-1) {
+            str.content += c.content
+          }
+        })
+        str.content = str.content.replace(/^\s+/, '')
+        this.push(str)
+      }
+      callback()
+    })).pipe(through2.obj(function (chunk, enc, callback) {
+      headings.push(chunk)
+      this.push(chunk)
+      callback()
+    }, function(callback){
+      if (then) then(headings)
+      callback()
+    })).pause();
+  stream.write(READMEContent)
+  return tokenized
+}
+
+function suggestAlternativeReadings(message, suggestions, then){
+  inquirer.prompt([{
+      type: "list",
+      name: "suggested",
+      message: message,
+      choices: suggestions
+    }], function( answers ) {
+    then(answers.suggested)
+  });
 }
